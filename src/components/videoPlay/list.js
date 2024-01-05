80e1264dafb6fae5e093e716077f5dd66c379594
wangkangzhang <1368504885@qq.com>
2024-01-05 16:27:50 +0800

    feat: 更新

--- src/components/videoPlay/list.js
+++ src/components/videoPlay/list.js
@@ -3,6 +3,11 @@ export const defaultList = [
     text: 'CCTV1',
     children: [
       {
+        text: '源',
+        id: 0,
+        url: 'https://cfss.cc/ds/ysp/qp.php?id=cctv1&key=31165&Cf.m3u8'
+      },
+      {
         text: '源1',
         id: 1,
         url: 'https://node1.olelive.com:6443/live/CCTV1HD/hls.m3u8'
@@ -10,7 +15,17 @@ export const defaultList = [
       {
         text: '源2',
         id: 2,
-        url: 'https://cfss.cc/ds/ysp/qp.php?id=cctv1&key=96697&Cf.m3u8'
+        url: 'http://120.196.232.124:8088/rrs03.hw.gmcc.net/PLTV/651/224/3221226667/1.m3u8'
+      },
+      {
+        text: '源3',
+        id: 3,
+        url: 'http://111.13.111.242/000000001000PLTV/123/224/3221235619/1.m3u8?HlsProfileId='
+      },
+      {
+        text: '源4',
+        id: 4,
+        url: 'https://iptv.luas.edu.cn/liverespath/b455c7a019d404d571655c457e0ee8a9440a7802/e0eea3f2cd-0-0-7b16046e28b9e794d06c5fbcc82d37b9/index.m3u8'
       }
     ]
   },
@@ -20,6 +35,11 @@ export const defaultList = [
       {
         text: '源1',
         id: 1,
+        url: 'http://111.13.111.242/000000001000PLTV/123/224/3221235639/1.m3u8?HlsProfileId='
+      },
+      {
+        text: '源2',
+        id: 2,
         url: 'https://cfss.cc/ds/ysp/qp.php?id=cctv2&key=96697&Cf.m3u8'
       }
     ]
