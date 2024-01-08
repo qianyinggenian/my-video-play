4da1c2e288386bf035f78d055d8745433e3fc9f4
wangkangzhang <1368504885@qq.com>
2024-01-08 17:17:18 +0800

    feat: 更新

--- src/components/videoPlay/index.vue
+++ src/components/videoPlay/index.vue
@@ -18,7 +18,7 @@
         <source :src="videoUrl" type="application/x-mpegURL"/>
       </video>
       <van-popup
-          v-model="show"
+          v-model="showList"
           round
           position="bottom"
           :style="{ height: '50%' }"
@@ -33,6 +33,49 @@
               @click-item="clickItem"
           >
           </van-tree-select>
+          <div style="margin: 16px;">
+            <van-button round block type="info" native-type="submit">确定</van-button>
+          </div>
+        </van-form>
+      </van-popup>
+      <van-popup
+          v-model="showSetting"
+          round
+          position="bottom"
+          :style="{ height: '50%' }"
+          @close="closePopupSetting"
+      >
+        <van-form @submit="onSubmitSetting">
+          <van-field
+              readonly
+              clickable
+              name="picker"
+              :value="sourcePath"
+              label="源来源"
+              placeholder="点击选择源来源"
+              @click="showPicker = true"
+          />
+          <van-cell-group v-if="sourcePath === '远程自定义'">
+            <van-field v-model="value" label="远程地址" placeholder="请输入远程地址" />
+          </van-cell-group>
+          <van-cell-group  v-if="sourcePath === '本地'">
+            <van-cell title="文件类型：.m3u,.m3u8,.txt,.json" />
+            <van-uploader
+                v-model="fileList"
+                :max-count="1"
+                accept=".m3u,.m3u8,.txt,.json"
+                :deletable="false"
+                :after-read="afterRead"
+            />
+          </van-cell-group>
+          <van-popup v-model="showPicker" position="bottom">
+            <van-picker
+                show-toolbar
+                :columns="columns"
+                @confirm="onConfirmPicker"
+                @cancel="showPicker = false"
+            />
+          </van-popup>
           <div style="margin: 16px;">
             <van-button round block type="info" native-type="submit">确定</van-button>
           </div>
@@ -56,11 +99,17 @@ export default {
   data () {
     return {
       active: 0,
-      show: false,
+      showList: false,
+      showSetting: false,
       imgUrl: imgUrl,
       activeId: 1,
       activeIndex: 0,
       updateVideoUrl: '',
+      sourcePath: '默认',
+      fileList: [],
+      value: '',
+      columns: ['默认', '本地', '远程自定义'],
+      showPicker: false,
       activeItem: {},
       // videoUrl: 'https://live-play.cctvnews.cctv.com/cctv/merge_cctv13.m3u8',
       // videoUrl: 'https://cfss.cc/ds/ysp/qp.php?id=cctv1&key=51397&Cf.m3u8',
@@ -100,6 +149,40 @@ export default {
     // this.fetchFileContent();
   },
   methods: {
+    onConfirmPicker (value) {
+      this.sourcePath = value;
+      this.showPicker = false;
+    },
+    /**
+     * @Description 选择文件后触发
+     * @author qianyinggenian
+     * @date 2024/01/08
+    */
+    afterRead (e) {
+      const file = e.file;
+      const fileType = file.name.substring(file.name.lastIndexOf('.')).toLocaleLowerCase();
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        this.getFileInfo(reader.result);
+      };
+      reader.readAsDataURL(file);
+    },
+
+    /**
+     * @Description 获取文件中的内容
+     * @author qianyinggenian
+     * @date 2024/01/08
+     */
+    getFileInfo (path) {
+      axios.get(path) // 这里的路径应根据实际情况修改
+        .then(response => {
+          this.fileContent = response.data;
+          console.log('fileContent', this.fileContent);
+        })
+        .catch(error => {
+          console.log('Error:', error);
+        });
+    },
     transformFn () {
       this.isFullTransform = !this.isFullTransform;
     },
@@ -209,7 +292,7 @@ export default {
           ],
           Menu: true,
           playbackRateMenuButton: true,
-          timeDivider: true,
+          timeDivider: false,
           currentTimeDisplay: true,
           playbackRate: true,
           remainingTimeDisplay: true,
@@ -241,11 +324,12 @@ export default {
         // this.addMenu();
         this.addButton();
         this.addButtonFull();
+        this.addSettingBtn();
         this.updateUrl();
         // this.fetchFileContent();
       });
     },
-    fn () {
+    customFullFn () {
       const app = document.querySelector('.video-container');
       const body = app;
       const width = body.clientWidth;
@@ -298,13 +382,21 @@ export default {
       this.player.controlBar.addChild(myMenuButton);
     },
     onSubmit () {
-      this.show = false;
+      this.showList = false;
       this.videoUrl = this.activeItem.url;
       this.player.src({
         type: 'application/x-mpegURL',
         src: this.videoUrl
       });
       this.player.play();
+    },
+    /**
+     * @Description 设置确定后触发
+     * @author qianyinggenian
+     * @date 2024/01/08
+    */
+    onSubmitSetting () {
+      this.showSetting = false;
     },
     /**
      * @Description 添加按钮
@@ -318,7 +410,7 @@ export default {
         controlText: '列表',
         clickHandler: (event) => {
           // 点击函数
-          this.show = true;
+          this.showList = true;
           this.updateVideoUrl = this.videoUrl;
         }
       });
@@ -338,17 +430,41 @@ export default {
           // 点击函数
 
           this.isFull = !this.isFull;
-          this.fn();
+          this.customFullFn();
         }
       });
       this.player.getChild('ControlBar').addChild(button);
     },
-    /** 关闭弹出触发
+    /**
+     * @Description 添加按钮
+     * @author wangkangzhang
+     * @date 2023/12/22
+     */
+    addSettingBtn () {
+      const Button = this.$videojs.getComponent('Button');
+      const button = new Button(this.player, {
+        className: 'vjs-visible-text custom-setting',
+        controlText: '设置',
+        clickHandler: (event) => {
+          // 点击函数
+          this.showSetting = !this.showSetting;
+        }
+      });
+      this.player.getChild('ControlBar').addChild(button);
+    },
+    /** 关闭列表弹窗触发
      * @param e
      * @param item
      */
     closePopup () {
-      this.show = false;
+      this.showList = false;
+    },
+    /** 关闭设置弹窗触发
+     * @param e
+     * @param item
+     */
+    closePopupSetting () {
+      this.showSetting = false;
     },
     setShowPercent (e, item) {
       console.log('e', e);
@@ -378,6 +494,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@mixin customStyle {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  text-align: center;
+  cursor: pointer;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 4em !important;
+}
 .video-container {
   width: 100%;
   height: 100%;
@@ -411,16 +539,8 @@ export default {
   margin-left: -1.75em !important;
 }
 ::v-deep .custom-list {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  text-align: center;
-  cursor: pointer;
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 4em !important;
+
+  @include customStyle;
   span {
     &:first-child {
       position: relative;
@@ -445,16 +565,17 @@ export default {
   }
 }
 ::v-deep .custom-full {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  text-align: center;
-  cursor: pointer;
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 4em !important;
+  //display: flex;
+  //align-items: center;
+  //justify-content: center;
+  //position: relative;
+  //text-align: center;
+  //cursor: pointer;
+  //margin: 0;
+  //padding: 0;
+  //height: 100%;
+  //width: 4em !important;
+  @include customStyle;
   span {
     &:first-child {
       position: relative;
@@ -465,6 +586,31 @@ export default {
       width: 100%;
       &::before {
         content: '\e6b2';
+        font-family: vant-icon;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+      }
+    }
+    &:last-child {
+      display: none;
+    }
+  }
+}
+::v-deep .custom-setting {
+  @include customStyle;
+  span {
+    &:first-child {
+      position: relative;
+      font-family: VideoJS;
+      font-weight: normal;
+      font-style: normal;
+      height: 100%;
+      width: 100%;
+      &::before {
+        content: '\e745';
         font-family: vant-icon;
         position: absolute;
         top: 0;
@@ -509,7 +655,7 @@ export default {
 ::v-deep .van-form {
   height: 100%;
   .van-tree-select {
-    height: calc(100% - 76px) !important;
+    height: calc(100% - 76px - 44px) !important;
     .van-tree-select__content {
       flex: 1 !important;
       -webkit-box-flex: 1 !important;
